7a84d2b887eb68bd6bddfc41f296661ddf2598f6
jaechu12 <kys20067765@gmail.com>
2025-05-05 17:43:40 +0900

    feat: put 기능 추가

--- src/main/java/com/example/bcsd/ArticleController.java
+++ src/main/java/com/example/bcsd/ArticleController.java
@@ -22,13 +22,24 @@ public class ArticleController {
         }
         return ResponseEntity.ok().body(article);
     }
-    
+
     @PostMapping("/article")
     public ResponseEntity<Article> postArticle(@RequestBody Article article) {
         long id = idGenerator.getAndIncrement();
         article.setId(id);
         articles.put(id, article);
         return ResponseEntity.status(HttpStatus.CREATED).body(article);
+    }
+
+    @PutMapping("/article/{id}")
+    public ResponseEntity<Article> putArticle(@PathVariable Long id, @RequestBody Article updatedArticle) {
+        Article article = articles.get(id);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+        article.setTitle(updatedArticle.getTitle());
+        article.setContent(updatedArticle.getContent());
+        return ResponseEntity.ok().body(article);
     }
 
 }
