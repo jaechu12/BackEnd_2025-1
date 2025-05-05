b45a0412a8b9a83e7a070fc8be2e69dbef8bdd50
jaechu12 <kys20067765@gmail.com>
2025-05-05 17:39:51 +0900

    feat: Article 추가

---
+++ src/main/java/com/example/bcsd/Article.java
@@ -0,0 +1,29 @@
+package com.example.bcsd;
+
+public class Article {
+        private Long id;
+        private String title;
+        private String content;
+
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getTitle() {
+                return title;
+        }
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public String getContent() {
+                return content;
+        }
+        public void setContent(String content) {
+                this.content = content;
+        }
+
+
+
+}
