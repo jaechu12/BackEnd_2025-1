5d2d1a35fa3e57909fba099c247a4aac0b0fba96
jaechu12 <kys20067765@gmail.com>
2025-05-12 18:16:31 +0900

    fix: Model 수정

--- src/main/java/com/example/bcsd/Article.java
+++ src/main/java/com/example/bcsd/Article.java
@@ -1,29 +1,80 @@
 package com.example.bcsd;
 
+import java.time.LocalDateTime;
+
 public class Article {
-        private Long id;
-        private String title;
-        private String content;
+    private Long id;
+    private String author;
+    private String board;
+    private String title;
+    private String content;
+    private LocalDateTime date;
+    private LocalDateTime revisedDate;
+
+    public Article(String title, String author, LocalDateTime date, String content) {
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.content = content;
+    }
 
 
-        public void setId(Long id) {
-                this.id = id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public String getTitle() {
-                return title;
-        }
-        public void setTitle(String title) {
-                this.title = title;
-        }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-        public String getContent() {
-                return content;
-        }
-        public void setContent(String content) {
-                this.content = content;
-        }
+    public void setBoard(String board) {
+        this.board = board;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setRevisedDate(LocalDateTime revisedDate) {
+        this.revisedDate = revisedDate;
+    }
+
+
+    public Long getID() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public LocalDateTime getRevisedDate() {
+        return revisedDate;
+    }
 
 
 }
