f235f6e47efce34b8e31f5deffaee4b68e73fce1
jaechu12 <kys20067765@gmail.com>
2025-05-12 18:14:17 +0900

    feat: /posts 기능 추가

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -12,7 +14,7 @@ public class HelloController {
 
     @GetMapping("/introduce")
     public String introduceName(@RequestParam(name = "name", required = false) String name, Model model) {
-        if (name==null){
+        if (name == null) {
             return "hello.html";
         }
         model.addAttribute("name", name);
@@ -21,9 +23,21 @@ public class HelloController {
 
     @GetMapping("/json")
     @ResponseBody
-    public introduce2 json(){
-        introduce2 intro = new introduce2(26,"허준기");
+    public IntroduceJson json() {
+        IntroduceJson intro = new IntroduceJson(26, "허준기");
         return intro;
     }
 
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+        List<Article> articleList = List.of(
+                new Article("제목0", "회원0", now, ""),
+                new Article("제목1", "회원1", now, "내용입니다!!"),
+                new Article("제목2", "회원2", now, "내용입니다!!내용입니다!!")
+        );
+        model.addAttribute("articles", articleList);
+        return "posts.html";
+    }
 }
