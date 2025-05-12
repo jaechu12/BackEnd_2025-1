6bc73709197807036ea55751d24ea219574376ed
jaechu12 <kys20067765@gmail.com>
2025-05-12 18:17:30 +0900

    feat: Board Model 추가

---
+++ src/main/java/com/example/bcsd/Board.java
@@ -0,0 +1,27 @@
+package com.example.bcsd;
+
+
+public class Board {
+    private Long ID;
+    private String board;
+
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+
+    public Long getID() {
+        return ID;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+
+}
