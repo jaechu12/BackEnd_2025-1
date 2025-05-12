2a494b28570bda2f4790b3c51eae42f5ed19e71c
jaechu12 <kys20067765@gmail.com>
2025-05-12 18:18:54 +0900

    feat: Member Model 추가

---
+++ src/main/java/com/example/bcsd/Member.java
@@ -0,0 +1,45 @@
+package com.example.bcsd;
+
+
+public class Member {
+    private Long ID;
+    private String name;
+    private String email;
+    private String password;
+
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String title) {
+        this.password = password;
+    }
+
+
+    public Long getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
