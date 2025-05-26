8e169c97b8a056a8ba096610f945118ad30802c6
jaechu12 <kys20067765@gmail.com>
2025-05-26 20:01:29 +0900

    refactor : 미사용 객체 제거

--- src/main/java/com/example/bcsd/ArticleController.java
+++ src/main/java/com/example/bcsd/ArticleController.java
@@ -3,7 +3,6 @@ package com.example.bcsd;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 
 
 @RestController
